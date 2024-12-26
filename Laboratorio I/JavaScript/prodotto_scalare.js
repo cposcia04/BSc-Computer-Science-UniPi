14ff92ed9cf38de8b6cdc4ce8589b26ade6e9872
Camilla Poscia <c.poscia@studenti.unipi.it>
2024-12-26 16:00:26 +0100

    Update prodotto_scalare.js

--- Laboratorio I/JavaScript/prodotto_scalare.js
+++ Laboratorio I/JavaScript/prodotto_scalare.js
@@ -1,12 +1,12 @@
 function prodotto_scalare(x,y) {
     let result = 0;
 
-  if(x.length !== y.length) {
+  if(x.length !== y.length) { //Se gli array hanno lunghezze diverse ritorna undefined
         return undefined;
     }
 
-    for(let i = 0; i < x.length; i++) {
-        result += x[i] * y[i];
+    for(let i = 0; i < x.length; i++) { 
+        result += x[i] * y[i]; 
     }
 
     return result;
