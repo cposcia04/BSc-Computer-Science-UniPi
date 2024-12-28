9eca47e86df3659929a315b8194f76487ebf71c4
Camilla Poscia <c.poscia@studenti.unipi.it>
2024-12-28 09:18:11 +0100

    Update minDepth.js

--- Laboratorio I/JavaScript/minDepth.js
+++ Laboratorio I/JavaScript/minDepth.js
@@ -2,22 +2,22 @@ function minDepth(T) {
     let depthSx = 0;
     let depthDx = 0;
     
-    if(T == undefined) {
+    if(T == undefined) { //albero vuoto: restituisco -1
         return -1;
     }
     
-    depthSx = minDepth(T.sx);
+    depthSx = minDepth(T.sx); //chiamate ricorsive su sottoalbero destro e sinistro
     depthDx = minDepth(T.dx);
     
-    if(depthSx == -1) {
+    if(depthSx == -1) {     //se non c'è il sottoalbero sinistro, la profondità è radice + profondità destra
         return 1 + depthDx;
     }
     
-    if(depthDx == -1) {
+    if(depthDx == -1) {    //se non c'è il sottoalbero destro, la profondità è radice + profondità sinistra
         return 1 + depthSx;
     }
     
-    if(depthSx < depthDx) {
+    if(depthSx < depthDx) { 
         return depthSx + 1;
     } else {
         return depthDx + 1;
