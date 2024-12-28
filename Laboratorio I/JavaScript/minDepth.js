bca1af1c69a19009d596ef9b1cfbcd8917b54699
Camilla Poscia <c.poscia@studenti.unipi.it>
2024-12-28 09:15:14 +0100

    Update minDepth.js

--- Laboratorio I/JavaScript/minDepth.js
+++ Laboratorio I/JavaScript/minDepth.js
@@ -1,1 +1,26 @@
+function minDepth(T) {
+    let depthSx = 0;
+    let depthDx = 0;
+    
+    if(T == undefined) {
+        return -1;
+    }
+    
+    depthSx = minDepth(T.sx);
+    depthDx = minDepth(T.dx);
+    
+    if(depthSx == -1) {
+        return 1 + depthDx;
+    }
+    
+    if(depthDx == -1) {
+        return 1 + depthSx;
+    }
+    
+    if(depthSx < depthDx) {
+        return depthSx + 1;
+    } else {
+        return depthDx + 1;
+    }
 
+}
