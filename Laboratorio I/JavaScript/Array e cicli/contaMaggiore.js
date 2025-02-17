d66a5dfd94927c9e1b35835268aa5c5ab0858926
Camilla Poscia <c.poscia@studenti.unipi.it>
2025-02-17 20:48:36 +0100

    Update contaMaggiore.js

--- Laboratorio I/JavaScript/Array e cicli/contaMaggiore.js
+++ Laboratorio I/JavaScript/Array e cicli/contaMaggiore.js
@@ -1,10 +1,10 @@
 function contaMaggiore(arr,soglia) {
 
-    let contatore = 0;
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] > soglia) {
-            contatore++;
+    let contatore = 0; //inizializzo il contatore a 0
+    for(let i = 0; i < arr.length; i++) { //itero su ogni elemento di arr
+        if(arr[i] > soglia) { //se elemento > soglia incrementa il contatore di +
+            contatore++; 
         }
     }
-    return contatore;
+    return contatore; //restituisce il numero 
 }
