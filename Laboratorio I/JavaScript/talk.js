c540485de59856a00c2bd06262f951f3ea057d6b
Camilla Poscia <c.poscia@studenti.unipi.it>
2025-02-28 20:01:25 +0100

    Update talk.js

--- Laboratorio I/JavaScript/talk.js
+++ Laboratorio I/JavaScript/talk.js
@@ -1,15 +1,3 @@
-/*Si consideri un sistema di gestione di talk, dove un generico talk è rappresentabile mediante la classe Talk, 
-i cui campi sono title (stringa), speaker (stringa) e duration (numerico, in minuti). 
-Il sistema prevede la possibilità di definire tre tipi specifici di Talk, ovvero Lecture, Seminar e Webinar:
-La classe Lecture rappresenta una lezione di un dato course, indicato nel costruttore, e fornisce un getter-setter teacher che corrisponde allo speaker del talk in questione.
-La classe Seminar rappresenta un seminario tenuto all'interno di un dato event, indicato nel costruttore.
-La classe Webinar che rappresenta anch'essa un seminario tenuto all'interno di un event, con tale evento però tenuto online.
-Si noti quindi che Webinar costituisce un caso specifico di Seminar.
-Si implementino le classi Talk, Lecture, Seminar e Webinar, organizzandole in modo da sfruttare al meglio l'ereditarietà. 
-Si implementi inoltre una funzione pickSeminar che, dato un array talks di Talk, restituisce il titolo del seminario più breve contenuto in talks. 
-Se non ci sono seminari, restituisce undefined.*/
-
-
 // Classe base Talk
 class Talk {
   constructor(title, speaker, duration) {
